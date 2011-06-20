8fef53904c75bcd94506948a77f2edbcc696c66b
daK76 <costa.ti@gmail.com>
2011-06-20 19:30:12 +0000

    Module config info Considering setting a config item to help on performance

--- config/config.php
+++ config/config.php
@@ -1,7 +1,17 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['module_config'] = array(
+	'name'			=> 'Langs',
+	'description'	=> 'A small module for editing language files in Bonfire..',
 	'menus'	=> array(
 		'developer'	=> 'languages/developer/_menu'
 	)
 );
+
+
+//--------------------------------------------------------------------
+// !CONFIG ITEMS 
+//--------------------------------------------------------------------
+
+// Set your default usual working langs here, for improved performance
+$config['langs.list'] = array();
